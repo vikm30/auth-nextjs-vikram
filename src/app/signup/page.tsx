bf1f22be8976327335e4656418b8c5c5a28e230a
vikram <vikrammukherjee8@gmail.com>
2025-05-14 14:26:52 -0400

    Updated Username Email Password Signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -30,6 +30,26 @@ export default function SignupPage() {
                 onChange = {(e) => setUser({...user, username: e.target.value})}
                 placeholder = "username"
                 />
+            <label htmlFor = "email">email</label>
+            <input
+            className = "p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                id = "uemail"
+                type = "text"
+                value = {user.email}
+                onChange = {(e) => setUser({...user, email: e.target.value})}
+                placeholder = "email"
+                />    
+            <label htmlFor = "password">password</label>
+            <input
+            className = "p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                id = "upassword"
+                type = "password"
+                value = {user.password}
+                onChange = {(e) => setUser({...user, password: e.target.value})}
+                placeholder = "password"
+                />        
             </div>
+            
+            
     )
 }
