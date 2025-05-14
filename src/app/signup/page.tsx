39647be1fd9fcf68bc4f6ff13f764a5142410b24
vikram <vikrammukherjee8@gmail.com>
2025-05-14 10:20:27 -0400

    First Snapshot

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import {useRouter} from "next/navigation";
+import {axios} from "axios";
+
+
+
+
+export default function SignupPage() {
+    const [user, setUser] = React.useState ({
+        email : "",
+        password : "",
+        username : "",
+    })
+    const onSignup = async () => {
+        
+    }
+
+    return (
+        <div className = "flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Signup</h1>
+            <hr />
+            <label htmlFor = "username">username</label>
+            <input
+                id = "username"
+                type = "text"
+                value = {user.username}
+                onChange = {(e) => setUser({...user, username: e.target.value})}
+                placeholder = "username"
+                />
+            </div>
+    )
+}
